89a8f1977c6b78c3033137d985d3ce8eb10dc896
Dierk Koenig <dierk.koenig@canoo.com>
2024-10-01 15:42:47 +0200

    test facililty enhanced

--- week03/lambda/lambdaTest.js
+++ week03/lambda/lambdaTest.js
@@ -2,35 +2,36 @@
 
 
 const ok = [];
+const test = result => ok.push(result);
 
 // // id
-// ok.push( id(1) === 1 );
-// ok.push( id(id) === id );
+// test( id(1) === 1 );
+// test( id(id) === id );
 //
 // // konst
-// ok.push( konst(42)(0) === 42 );
-// ok.push( konst(42)(1) === 42 );
-// ok.push( konst(42)(null) === 42 );
+// test( konst(42)(0) === 42 );
+// test( konst(42)(1) === 42 );
+// test( konst(42)(null) === 42 );
 //
 // // kite
-// ok.push( snd(null)(42) === 42 );
+// test( snd(null)(42) === 42 );
 //
 // // true
 //
-// ok.push( T(1)(0) === 1 );
-// ok.push( F(1)(0) === 0 );
+// test( T(1)(0) === 1 );
+// test( F(1)(0) === 0 );
 //
 // // and
-// ok.push( and(F)(F) === F );
-// ok.push( and(T)(F) === F );
-// ok.push( and(F)(T) === F );
-// ok.push( and(T)(T) === T );
+// test( and(F)(F) === F );
+// test( and(T)(F) === F );
+// test( and(F)(T) === F );
+// test( and(T)(T) === T );
 //
 // // or
-// ok.push( or(F)(F) === F );
-// ok.push( or(T)(F) === T );
-// ok.push( or(F)(T) === T );
-// ok.push( or(T)(T) === T );
+// test( or(F)(F) === F );
+// test( or(T)(F) === T );
+// test( or(F)(T) === T );
+// test( or(T)(T) === T );
 
 // flip
 // flip(f)(x)(y) = f(y)(x)
@@ -42,27 +43,27 @@ const ok = [];
 // // Pair
 //
 // const dierk = Pair("Dierk")("König"); // immutable
-// ok.push( dierk(firstname) === "Dierk");
-// ok.push( dierk(lastname)  === "König");
+// test( dierk(firstname) === "Dierk");
+// test( dierk(lastname)  === "König");
 //
 // const tdierk = Triple("Dierk")("König")(50); // immutable
-// ok.push( tdierk(tfirstname) === "Dierk");
-// ok.push( tdierk(tlastname)  === "König");
-// ok.push( tdierk(tage)       === 50);
+// test( tdierk(tfirstname) === "Dierk");
+// test( tdierk(tlastname)  === "König");
+// test( tdierk(tage)       === 50);
 //
 // // tuple
 // const [Person, fn, ln, ag] = Tuple(3);
 // const person = Person("Dierk")("König")(50);
-// ok.push( person(fn) === "Dierk");
-// ok.push( person(ln) === "König");
-// ok.push( person(ag) === 50);
+// test( person(fn) === "Dierk");
+// test( person(ln) === "König");
+// test( person(ag) === 50);
 //
 // // composed Tuple
 //
 // const [Team, lead, deputy] = Tuple(2);
 // const team = Team (person) (Person("Roger")("Federer")(35));
-// ok.push( team(lead)(fn)   === "Dierk");
-// ok.push( team(deputy)(ln) === "Federer");
+// test( team(lead)(fn)   === "Dierk");
+// test( team(deputy)(ln) === "Federer");
 //
 // // Pair equal
 //
@@ -92,10 +93,10 @@ const ok = [];
 //         ( account => account.name + " " + account.number )
 //         ( person  => "pal: " + person(fn) );
 //
-// ok.push( doPay(cash) === "paid cash");
-// ok.push( doPay(card) === "credit card 0000-1111-2222-3333");
-// ok.push( doPay(invo) === "Roger 4711");
-// ok.push( doPay(pal ) === "pal: Dierk");
+// test( doPay(cash) === "paid cash");
+// test( doPay(card) === "credit card 0000-1111-2222-3333");
+// test( doPay(invo) === "Roger 4711");
+// test( doPay(pal ) === "pal: Dierk");
 
 
 
